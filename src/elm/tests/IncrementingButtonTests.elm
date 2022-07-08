e7d4b5010b723c3ec5edec6dc563842cc7cd88e4
Gregory Albouy <gregoryalbouy75@gmail.com>
2022-07-08 20:58:57 +0200

    test(elm, IncrementingButton): write test case for increment

--- src/elm/tests/IncrementingButtonTests.elm
+++ src/elm/tests/IncrementingButtonTests.elm
@@ -2,6 +2,7 @@ module IncrementingButtonTests exposing (suite)
 
 import Expect exposing (Expectation)
 import Html exposing (Html, button, text)
+import Html.Events exposing (onClick)
 import IncrementingButton
 import Test exposing (Test, describe, test)
 
@@ -10,20 +11,31 @@ suite : Test
 suite =
     describe "IncrementingButton"
         [ testInitialState
+        , testIncrement
         ]
 
 
 testInitialState : Test
 testInitialState =
     test "initial state is 0" <|
-        expectContent "Clicked 0 times" IncrementingButton.view
+        expectContent "Clicked 0 times" (IncrementingButton.view 0)
 
 
-expectContent : String -> Html msg -> () -> Expectation
+testIncrement : Test
+testIncrement =
+    let
+        ( count, _ ) =
+            IncrementingButton.update IncrementingButton.Increment 1
+    in
+    test "increments count on click" <|
+        expectContent "Clicked 2 times" (IncrementingButton.view count)
+
+
+expectContent : String -> Html IncrementingButton.Msg -> () -> Expectation
 expectContent content buttonView =
     let
-        expectedView : Html msg
+        expectedView : Html IncrementingButton.Msg
         expectedView =
-            button [] [ text content ]
+            button [ onClick IncrementingButton.Increment ] [ text content ]
     in
     \() -> Expect.equal expectedView buttonView
