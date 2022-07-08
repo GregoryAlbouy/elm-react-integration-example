0da487d5bb340ca6d0ab91b2af3b11d4af599b1b
Gregory Albouy <gregoryalbouy75@gmail.com>
2022-07-08 20:59:18 +0200

    feat(elm, IncrementingButton): implement increment

--- src/elm/src/IncrementingButton.elm
+++ src/elm/src/IncrementingButton.elm
@@ -1,8 +1,35 @@
-module IncrementingButton exposing (view)
+module IncrementingButton exposing (Model, Msg(..), main, update, view)
 
+import Browser
 import Html exposing (Html, button, text)
+import Html.Events exposing (onClick)
 
 
-view : Html msg
-view =
-    button [] [ text "Clicked 0 times" ]
+type alias Model =
+    Int
+
+
+type Msg
+    = Increment
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg count =
+    case msg of
+        Increment ->
+            ( count + 1, Cmd.none )
+
+
+view : Model -> Html Msg
+view count =
+    button [ onClick Increment ] [ text ("Clicked " ++ String.fromInt count ++ " times") ]
+
+
+main : Program () Model Msg
+main =
+    Browser.element
+        { init = \_ -> ( 0, Cmd.none )
+        , view = view
+        , update = update
+        , subscriptions = \_ -> Sub.none
+        }
